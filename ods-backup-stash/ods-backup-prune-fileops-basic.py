d778cf701641bcbe62c05339870d24835311b8d4
root <root@reddingsboei.surfdomeinen.nl>
2018-06-23 20:39:33 +0200

    Crude initial form of ods-backup-prune.  Derives the scheme of things.  - Derives .midnight summary names and their origins  - Derives .merges   summary names and their origins  - Does not yet incorporate the backend module for diff/merge fileops  - Does not yet remove files (or setup timers for it)  - Does not yet protect files from removal by locking them  - Does not yet produce the actual files on disk  - Does not yet invoke cmd_have_xxx() or cmd_drop_xxx()  - Does not yet parse periods very well (meaning, very pragmatic)  - Does not yet produce historic files or their production rules  - Does not yet recognise a repeated .midnight summary for simple linking

---
+++ ods-backup-stash/ods-backup-prune-fileops-basic.py
@@ -0,0 +1,6 @@
+# ods-backup-prune-no-hsm.py -- Modular support with no HSM specifics.
+#
+# This module rejects all operations on HSM backup files.  It serves
+# as a fallback for ods-backup-prune when no HSM plugin is available.
+#
+# From: Rick van Rein <rick@openfortress.nl>
